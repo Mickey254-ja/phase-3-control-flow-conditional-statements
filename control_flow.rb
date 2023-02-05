3b30a5c583ef6d0e7565eca1a4c680b41b2915bf
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-02-06 01:08:27 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,55 @@
+require 'pry'
+
 def admin_login(username, password)
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+     "Access granted"
+  else
+    "Access denied"
+  end 
   # your code here
 end
 
 def hows_the_weather(temperature)
+  if temperature < 40
+     "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+     "It's a little chilly out there!"
+  elsif temperature > 85
+     "It's too dang hot out there!"
+  else
+     "It's perfect out there!"
+  end
   # your code here
 end
 
 def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else
+    return num
+  end
   # your code here
 end
 
 def calculator(operation, num1, num2)
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+     num1 - num2
+  when "*"
+     num1 * num2
+  when "/"
+    num1.to_f / num2.to_f
+  else
+    puts "Invalid operation!"
+     nil
+  end
   # your code here
 end
 
+binding.pry
