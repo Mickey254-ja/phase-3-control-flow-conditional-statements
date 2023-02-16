b6b39b7f633781633a4ef86c3893e30c11d3b0d8
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-02-16 09:39:22 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 def admin_login(username, password)
   if (username == "admin" || username == "ADMIN") && password == "12345"
@@ -52,4 +52,4 @@ def calculator(operation, num1, num2)
   # your code here
 end
 
-binding.pry
+
